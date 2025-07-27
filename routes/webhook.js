b5b1259d2abf5c8ef9fbb4248371297abbbfc569
Albert Augustine <albertaugustine1884@gmail.com>
2025-07-27 18:54:03 +0530

    Update webhook.js

--- routes/webhook.js
+++ routes/webhook.js
@@ -3,23 +3,22 @@ const router = express.Router();
 const submissionQueue = require('../queue/queue');
 
 router.post('/', async (req, res) => {
-  const { name, commitCount, timestamp, files } = req.body;
+  const { name, commitCount, timestamp, filename, content } = req.body;
 
-  if (!name || !timestamp || !files || !Array.isArray(files)) {
-    return res.status(400).json({ error: 'Missing fields' });
+  if (!name || !timestamp || !filename || !content) {
+    return res.status(400).json({ error: 'Missing required fields' });
   }
 
   try {
     await submissionQueue.add('code-submissions', {
-        name,
-        commitCount,
-        timestamp,
-        content,
-        filename,
-      });
-    }
+      name,
+      commitCount,
+      timestamp,
+      filename,
+      content
+    });
 
-    return res.status(200).json({ message: 'Queued all files successfully' });
+    return res.status(200).json({ message: 'Queued successfully' });
   } catch (err) {
     console.error(err);
     return res.status(500).json({ error: 'Queueing failed' });
